5f7d8c899045eb96927303b758655422e0f6397f
esraelif <elifesratunca@gmail.com>
2024-02-25 13:36:31 +0100

    carmoved

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import TouchAppIcon from '@mui/icons-material/TouchApp';
+import TimeToLeaveIcon from '@mui/icons-material/TimeToLeave';
 
 function App() {
+  const [carPosition, setCarPosition] = useState({ x: 250, y: 250 });
+  const [carRotation, setCarRotation] = useState(0);
+  const [backgroundColor, setBackgroundColor] = useState('gray');
+
+  const handleUpClick = () => {
+    setCarRotation(prevRotation => prevRotation + 180);
+    setBackgroundColor('green'); // Yukarı butonuna basıldığında arka plan rengini yeşil yap
+  };
+
+  const handleDownClick = () => {
+    setCarRotation(prevRotation => prevRotation - 180);
+    setBackgroundColor('purple'); // Aşağı butonuna basıldığında arka plan rengini mor yap
+  };
+
+  const handleKeyDown = (event) => {
+    const moveDistance = 2500; // Klavye tuşlarına basıldığında arabanın hareket edeceği mesafe
+    switch (event.key) {
+      case 'ArrowRight':
+        setCarPosition(prevPosition => ({ ...prevPosition, x: prevPosition.x + moveDistance }));
+        setBackgroundColor('green'); // Sağa hareket ettiğinde arka plan rengini yeşil yap
+        break;
+      case 'ArrowLeft':
+        setCarPosition(prevPosition => ({ ...prevPosition, x: prevPosition.x - moveDistance }));
+        setBackgroundColor('green'); // Sola hareket ettiğinde arka plan rengini yeşil yap
+        break;
+      default:
+        break;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onKeyDown={handleKeyDown} tabIndex="0">
+      <div className="top-icon" onClick={handleUpClick}>
+        <TouchAppIcon style={{ fontSize: 64 }} />
+      </div>
+      <div
+        className="car"
+        style={{
+          left: carPosition.x,
+          top: carPosition.y,
+          transform: `rotate(${carRotation}deg)`,
+          backgroundColor: backgroundColor,
+          transition: 'transform 1s ease, background-color 1s ease'
+        }}>
+        <TimeToLeaveIcon style={{ fontSize: 64 }} />
+      </div>
+      <div className="bottom-icon" onClick={handleDownClick}>
+        <TouchAppIcon style={{ fontSize: 64 }} />
+      </div>
     </div>
   );
 }
